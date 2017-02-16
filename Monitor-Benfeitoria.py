ecf04b37086032f6972144dfa2e51bb016b3ddec
PalmaAnderson <palma.anderson1@gmail.com>
2017-02-16 16:14:25 -0200

    Create Monitor-Benfeitoria.py
    versao super basica, testes internos

---
+++ Monitor-Benfeitoria.py
@@ -0,0 +1,38 @@
+import urllib.request
+import json
+from pprint import pprint
+import winsound
+import time
+
+
+oldMoney = 0
+
+def playSound():        
+     winsound.Beep(300, 200)
+     winsound.Beep(500, 300)
+     winsound.Beep(300, 200)
+     winsound.Beep(200, 300) 
+     winsound.Beep(400, 300)
+
+def proc(resp, locate):
+    money=""
+    for x in range(locate+48,locate+55):
+        money=money+resp[x]
+        #print (resp[x])
+    return money
+while (1):
+    
+    resp = urllib.request.urlopen("http://benfeitoria.com/startupz2").read()
+    locate = str(resp).find("<span>R$</span>")
+    #print ("found at ")
+    #print (locate)
+    money= proc(str(resp), locate)
+    #print ("money = ")
+    if money != oldMoney:
+        print ("Nova DoaCao")
+        playSound()
+        
+    print (money)
+        
+    oldMoney = money
+    time.sleep( 10 )
